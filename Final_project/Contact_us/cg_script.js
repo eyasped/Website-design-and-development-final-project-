7cdc725da35e1b1284f4dff71e4dcd28f65063e1
eyasped <47040062+eyasped@users.noreply.github.com>
2021-10-19 17:58:17 -0400

    website design and development class final project

---
+++ Final_project/Contact_us/cg_script.js
@@ -0,0 +1,12 @@
+
+window.onload = init;
+
+function init() {
+   document.forms[0].onsubmit = function() {
+      if (this.checkValidity()) alert("Data passes initial validation tests");
+      return false;
+   }
+   
+}
+
+  
